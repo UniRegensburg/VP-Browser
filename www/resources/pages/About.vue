0f867494af9625c15606e8f33c9affae0a70f5a2
Lucas Haberl <Lucas.Haberl@stud.uni-regensburg.de>
2019-06-26 21:48:52 +0200

    routing geht nicht
    pages ordner nicht in verwendung
    home seite funktioniert, about gibt cannot get about, notFound wird nie verwendet
    
    versucht wurde dieses Tutorial https://vuejs.org/v2/guide/routing.html ; https://github.com/chrisvfritz/vue-2.0-simple-routing-example umzusetzen.
    
    Tutorial funktioniert am PC, ebenso wie der Server für dieses Projekt

---
+++ www/resources/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <main-layout>
+    <p>About page</p>
+	<p>stuff</p>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
