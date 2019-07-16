eb7137198bb63c8c80cc152f0c1bd6d76bbb61bf
Lucas Haberl <Lucas.Haberl@stud.uni-regensburg.de>
2019-07-16 20:27:33 +0200

    profil 4. option und speichern knopf

--- vp-browser/src/views/Profil.vue
+++ vp-browser/src/views/Profil.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <h1>Profil</h1>
+    <h1 id="profilH1">Profil</h1>
     <p>Email Benachrichtigungen</p>
     <div id="benachIn">
       <input type="radio" name="email" value="0">Alle<br>
       <input type="radio" name="email" value="1">nur neue Versuche<br>
       <input type="radio" name="email" value="2">nur Anmeldebestätigung<br>
+      <input type="radio" name="email" value="3">Keine<br>
     </div>
+    <button id="benachButton">Einstellungen speichern</button>
     <br>
     <router-link to="/mv">meine Versuche</router-link>
   </div>
@@ -19,33 +21,50 @@
 
   import Firebase from '../js/Firebase';
 
-  let myFirebase = new Firebase();
+  let myFirebase = new Firebase(),
+  benachValue;
 
   function loadProfil() {
-    setBenachChecked();
+    setH1();
     setBenachListener();
+    setBenachChecked();
+    setSpeichern();
+  }
+
+  function setH1() {
+    let h1 = document.getElementById("profilH1");
+    h1.innerText = h1.innerText + " - " + sessionStorage.getItem("nutzerName");
+  }
+
+  function setBenachListener() {
+    let boxes = document.getElementsByName("email");
+    for (let i=0; i<boxes.length; i++) {
+      boxes[i].addEventListener("input", function() {
+        if (boxes[i].checked) {
+          benachValue = boxes[i].value;
+        }
+      })
+    }
   }
 
   function setBenachChecked() {
-    let benachIndex = sessionStorage.getItem("benach"),
-    benachDiv = document.getElementById("benachIn").children;
+      let benachValue = sessionStorage.getItem("benach"),
+      boxes = document.getElementsByName("email");
 
-    for (let i=0; i<benachDiv.length; i++) {
-      if (i == 2*benachIndex) { //2* weil input br
-        benachDiv[i].checked = true;
+      for (let i=0; i<boxes.length; i++) {
+        if (boxes[i].value == benachValue) {
+          boxes[i].checked = true;
+        }
       }
-    }
   }
 
-  function setBenachListener() { //save button?
-  let benachDiv = document.getElementById("benachIn").children;
+  function setSpeichern() { //save button?
+    let benachButton = document.getElementById("benachButton");
 
-    for (let i=0; i<benachDiv.length; i+=2) {
-      benachDiv[i].addEventListener("input", function() {
-        myFirebase.setBenachDB(benachDiv[i].value);
-        sessionStorage.setItem("benach", benachDiv[i].value);
-      });
-    }
+    benachButton.addEventListener("click", function() {
+      myFirebase.setBenachDB(benachValue);
+      sessionStorage.setItem("benach", benachValue);
+    });
   }
 
   export default {
