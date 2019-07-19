cbee54aa4169a6752001c1b63e3d14efccde3cda
HaberlPre <lucas.haberl@stud-uni-regensburg.de>
2019-07-19 14:51:10 +0200

    old versions deleted

--- old/index.js
+++
@@ -1,9 +0,0 @@
-const AppServer = require("./lib/AppServer.js");
-
-AppServer.start({
-	port: 8080,
-	path: "www",
-	dbURL: "/db",
-	dbConfig: "db/config.json",
-	dbPath: "db/",
-});
--- old/www/resources/pages/pageManager.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env browser */
-
-import home from "./home.js";
-
-// Create and export routes to componentes
-
- export default [
-   { "": home.home },
- ];
